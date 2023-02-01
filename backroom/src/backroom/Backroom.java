2bb42b1a68939ecf3e0bfadd8375c5faef070d84
Artigala Quentin <quentin.artigala@etu.u-bordeaux.fr>
2023-02-01 10:19:40 +0100

    update

--- backroom/src/backroom/Backroom.java
+++ backroom/src/backroom/Backroom.java
@@ -261,15 +261,9 @@ public class Backroom {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-<<<<<<< backroom/src/backroom/Backroom.java
-        
-=======
         afficherMenu();
-        System.out.println("salut");
         System.out.println(GRENOUILLE_GAGNE);
         System.out.println(GRENOUILLE_PERDU);
         System.out.println(GRENOUILLE_MUR);
->>>>>>> backroom/src/backroom/Backroom.java
     }
 }
